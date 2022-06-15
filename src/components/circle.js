e6da1629cf3c9c5f7be950180fa5802fa68aa527
anand <anandpasodi96@gmail.com>
2022-06-15 11:19:20 +0530

    initial commit

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,14 @@
 // import using require
+var shape =require('./shape');
 
 // declare class
+class Circle extends shape{
+    calculateArea() {
+    return 3.14*4*4;
+    }
 
+}
+module.exports=Circle;
+// export class using module.exports
 
 // export class using module.exports
